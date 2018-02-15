94927c1a09af2fd7998d84faab8df001476c0825
RohitThorat95 <thorat.rohit95@gmail.com>
2018-02-15 20:11:25 +0530

    first commit

---
+++ angular/controller/myController.js
@@ -0,0 +1,77 @@
+myApp.controller('myController', function($scope){
+$scope.items = [
+ {
+   icon :"far fa-calendar-alt fa-2x roundIcon text-muted",
+   heading : "Blog Post",
+   words : "400-500 words"
+ },
+ {
+   icon :"fas fa-file fa-2x roundIcon text-muted",
+   heading : "White Paper",
+   words : "2500-3000 words"
+ },
+ {
+   icon :"fas fa-laptop fa-2x roundIcon text-muted",
+   heading : "Website Content",
+   words : "250-300 words"
+ },
+ {
+   icon :"fas fa-print fa-2x roundIcon text-muted",
+   heading : "Press Release",
+   words : "350-450 words"
+ },
+ {
+   icon :"fas fa-file-alt fa-2x roundIcon text-muted",
+   heading : "Product Description",
+   words : "150-200 words"
+ },
+ {
+   icon :"fas fa-mobile-alt fa-2x roundIcon text-muted",
+   heading : "Social Media",
+   words : "30-35 words"
+ },
+ {
+   icon :"fas fa-book fa-2x roundIcon text-muted",
+   heading : "Book/Story Writing",
+   words : "500-1000 words"
+ },
+ {
+   icon :"fas fa-film fa-2x roundIcon text-muted",
+   heading : "Video Script",
+   words : "1000-4000 words"
+ },
+ {
+   icon :"fas fa-paste fa-2x roundIcon text-muted",
+   heading : "Newsletter",
+   words : "400-500 words"
+ },
+ {
+   icon :"fas fa-edit fa-2x roundIcon text-muted",
+   heading : "Brochure Content",
+   words : "850-1000 words"
+ },
+ {
+   icon :"fas fa-wrench fa-2x roundIcon text-muted",
+   heading : "Custom Content",
+   words : "250-1000 words"
+ },
+ {
+   icon :"fas fa-trophy fa-2x roundIcon text-muted",
+   heading : "Contest",
+   words : "10-20 words"
+ }
+];
+
+$scope.twoItems = [
+ {
+   icon :"fas fa-bookmark fa-2x roundIcon text-muted",
+   heading : "Order from Template",
+   words : "You can give an order from templates"
+ },
+ {
+   icon :"fas fa-file-excel fa-2x roundIcon text-muted",
+   heading : "Order via Spreadsheets",
+   words : "It's useful when giving bulk orders"
+ }
+];
+})
